e46b7f63e1312ae91793d7de5ca97170d4917689
willy <willybalt.2017@gmail.com>
2026-02-21 17:42:35 -0400

    api.js consumo con fetch

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { getData } from './api/api'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  getData()
+ 
   return (
     <>
       <div>
---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+export async function getData() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/');
+
+    if (!response.ok) {
+        throw new Error("Error en la petición");
+    }
+
+    const data = await response.json();
+    console.log(data)
+    return data;
+}
